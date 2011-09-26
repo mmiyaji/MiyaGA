16982fff77404477a98e34bf0b90121b34c0d203
mmiyaji <ruhenheim1223@gmail.com>
2011-09-26 23:29:36 +0900

    とりあえずのコミット。まだ最適化してないよ

--- Parameta.py
+++ Parameta.py
@@ -12,7 +12,11 @@ import random, time
 class Parameta:
 	"""docstring for Parameta"""
 	def __init__(self, random_seed = time.time(), gene_length=100, dimention=2, population_size=10, max_generation=100,
-					scaling_method="liner", selection_method="", crossover_method="", mutation_method="", mutation_rate=0.1):
+					scaling_method="liner", 
+					selection_method="roulette", tournament_size = 5, 
+					crossover_method="single", crossover_rate=1,
+					mutation_method="normal", mutation_rate=0.1,
+					eliet_method="save", eliet_rate=1):
 		random.seed(random_seed)
 		self.gene_length = gene_length
 		self.dimention = dimention
@@ -21,8 +25,10 @@ class Parameta:
 		self.scaling_method = scaling_method
 		self.selection_method = selection_method
 		self.crossover_method = crossover_method
+		self.crossover_rate = crossover_rate
 		self.mutation_method = mutation_method
 		self.mutation_rate = mutation_rate
+		self.eliet_method = eliet_rate
 		
 def main():
 	pass
