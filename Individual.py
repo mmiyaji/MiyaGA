a388be970ecdf4a73b308f5391575ce29192e623
mmiyaji <ruhenheim1223@gmail.com>
2011-09-28 04:29:08 +0900

    test

--- Individual.py
+++ Individual.py
@@ -14,12 +14,34 @@ class Individual:
 	"""docstring for Individual"""
 	def __init__(self, id = 0, parameta = Parameta(), gene = None):
 		self.id = id
+		self.eval = 0
 		self.parameta = parameta
 		if gene:
 			self.gene = gene
 		else:
 			self.gene = [random.choice([0,1]) for i in xrange(parameta.gene_length*parameta.dimention)]
-		
+	
+	def gray_to_binary(self):
+		binary = []
+		binary.append(self.gene[0])
+		for i in xrange(1,len(self.gene)):
+			binary.append(binary[i-1]^self.gene[i])
+		return binary
+	
+	def gray_to_decimal(self):
+		binary = self.gray_to_binary
+		return self.binary_to_decimal(binary)
+	
+	@staticmethod
+	def binary_to_decimal(binary):
+		decimal = 0
+		length = len(binary)
+		for i in range(length):
+			decimal += binary[i]*(2**(length-i-1))
+		return decimal
+	
+	
+	
 def main():
 	individual = Individual()
 	print individual.gene
